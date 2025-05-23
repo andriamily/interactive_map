6179c874b74514aa9208ae23eadceb70fab410ed
andriamily <radoandriamily@gmail.com>
2025-05-23 05:20:47 +0300

    Add files via upload

---
+++ leaflet-ct.js
@@ -0,0 +1,162 @@
+var _0x1113c1 = _0x4e1d;function _0x403b() {var _0xe8d494 = ['setContent', 'map-tooltip', 'backgroundColor', '4902522nnQHKX', 'borderRadius', 'Ce\x20que\x20vous\x20voulez\x20montrer\x20avec\x20une\x20petite\x20description\x20comme\x20ça!', 'error', '50%', 'mapportfolio', '#008000', 'zIndex', 'left', 'white', 'position', 'properties', 'grab', '#66CC66', 'default', 'textAlign', 'top', '#FFAA00', 'absolute', 'map', 'getNorthEast', 'DOMContentLoaded', 'border', 'appendChild', 'tileLayer', 'catch', '12GJVZcx', 'Descriptions\x20\x20|\x20Votre\x20projet', 'translate(-50%,\x20-50%)', 'transform', 'getBounds', '1845262WWrAlV', '\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20#', 'servicesmapM3', 'div', 'getElementById', 'nom', 'width', '903144PRhYpU', '9712rWsbZO', 'lim_region_c.geojson', 'removeChild', 'createElement', 'lat', 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 'parentNode', 'then', '495711AnMkeK', 'openOn', 'Erreur\x20de\x20chargement\x20du\x20GeoJSON:', '16882040pOHBAr', 'control', '489744wIZzxc', 'create', 'cursor', '#3399FF', 'bindPopup', 'Texte\x20de\x20votre\x20choix\x20|\x20Votre\x20projet', 'color', '5DTFRJb', '10px', 'setLatLng', 'json', 'disable', 'lim_region_a.geojson', 'DomUtil', 'Tiles\x20©\x20Esri\x20&mdash;\x20Source:\x20Esri,\x20USGS,\x20NOAA', '5068paKPCL', 'Région\x20ATSIMO\x20ANDREFANA\x20|\x20Votre\x20projet', 'padding', 'click', 'addEventListener', '&copy;\x20<a\x20href=\x22https://www.openstreetmap.org/copyright\x22>OpenStreetMap</a>\x20contributors', 'intermap', 'addTo', '\x20.leaflet-touch\x20.leaflet-bar\x20{\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20display:\x20none\x20!important;\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20}\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20', 'head', 'forEach', '40%', 'black', 'scrollWheelZoom', '1px\x20solid\x20black', 'style', '#FF0000', 'lng', 'lim_region_b.geojson', 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', 'layers', 'wheel', 'Cliquez\x20pour\x20activer\x20la\x20carte.', 'innerHTML', 'geoJSON'];_0x403b = function() {return _0xe8d494;};
+return _0x403b();}function _0x4e1d(_0x28d640, _0x2e2917) {var _0x4e1d44 = _0x403b();
+return _0x4e1d = function(_0x216fab, _0x4557b7) {_0x216fab = _0x216fab - 0xe7;
+        var _0x369806 = _0x4e1d44[_0x216fab];
+        return _0x369806;
+    }
+    ,
+    _0x4e1d(_0x28d640, _0x2e2917);
+}
+(function(_0x233ec6, _0x190005) {
+    var _0x13a44b = _0x4e1d
+      , _0x35eff0 = _0x233ec6();
+    while (!![]) {
+        try {
+            var _0x3cd837 = -parseInt(_0x13a44b(0xf2)) / 0x1 + -parseInt(_0x13a44b(0xeb)) / 0x2 + parseInt(_0x13a44b(0xfb)) / 0x3 * (parseInt(_0x13a44b(0x145)) / 0x4) + parseInt(_0x13a44b(0x107)) / 0x5 * (-parseInt(_0x13a44b(0x12b)) / 0x6) + -parseInt(_0x13a44b(0x10f)) / 0x7 * (-parseInt(_0x13a44b(0xf3)) / 0x8) + parseInt(_0x13a44b(0x100)) / 0x9 + parseInt(_0x13a44b(0xfe)) / 0xa;
+            if (_0x3cd837 === _0x190005)
+                break;
+            else
+                _0x35eff0['push'](_0x35eff0['shift']());
+        } catch (_0x3beb29) {
+            _0x35eff0['push'](_0x35eff0['shift']());
+        }
+    }
+}(_0x403b, 0x73d25),
+document[_0x1113c1(0x113)](_0x1113c1(0x140), function() {
+    var _0x51ad37 = _0x1113c1
+      , _0x42e816 = L[_0x51ad37(0x143)](_0x51ad37(0xf8), {
+        'attribution': _0x51ad37(0x114)
+    })
+      , _0x49f2a1 = L[_0x51ad37(0x143)](_0x51ad37(0x122), {
+        'attribution': _0x51ad37(0x10e)
+    })
+      , _0x487a03 = L[_0x51ad37(0x13e)](_0x51ad37(0x130), {
+        'center': [-18.766947, 48.869107],
+        'zoom': 5.33,
+        'layers': [_0x42e816],
+        'scrollWheelZoom': ![]
+    })
+      , _0x9aff = {
+        'Carte\x20Standard': _0x42e816,
+        'Satellite': _0x49f2a1
+    };
+    L[_0x51ad37(0xff)][_0x51ad37(0x123)](_0x9aff)['addTo'](_0x487a03),
+    L['marker']([-18.85, 47.52])['addTo'](_0x487a03)[_0x51ad37(0x104)](_0x51ad37(0x12d))['openPopup'](),
+    fetch(_0x51ad37(0x121))[_0x51ad37(0xfa)](_0x5649ba => _0x5649ba[_0x51ad37(0x10a)]())[_0x51ad37(0xfa)](_0x54c830 => {
+        var _0x197e53 = _0x51ad37;
+        let _0x2e8e4a = L['geoJSON'](_0x54c830, {
+            'style': function(_0x2255f2) {
+                var _0xdfb568 = _0x4e1d;
+                return {
+                    'color': _0xdfb568(0x131),
+                    'weight': 0x2,
+                    'fillColor': _0xdfb568(0x138),
+                    'fillOpacity': 0.1
+                };
+            },
+            'onEachFeature': function(_0x3a8212, _0x535183) {
+                var _0x56f94d = _0x4e1d;
+                _0x535183[_0x56f94d(0x104)](_0x3a8212['properties'][_0x56f94d(0xf0)] || _0x56f94d(0xe7));
+            }
+        })[_0x197e53(0x116)](_0x487a03)
+          , _0x316350 = _0x2e8e4a[_0x197e53(0xea)]()[_0x197e53(0x13f)]()
+          , _0x52eea6 = [_0x316350[_0x197e53(0xf7)] - 0.52, _0x316350[_0x197e53(0x120)] - 1.02];
+        L['popup']()[_0x197e53(0x109)](_0x52eea6)[_0x197e53(0x128)](_0x197e53(0xe7))[_0x197e53(0xfc)](_0x487a03);
+    }
+    )[_0x51ad37(0x144)](_0x2cd8a7 => console[_0x51ad37(0x12e)](_0x51ad37(0xfd), _0x2cd8a7)),
+    fetch(_0x51ad37(0xf4))['then'](_0x5172ca => _0x5172ca[_0x51ad37(0x10a)]())['then'](_0x68e3f3 => {
+        var _0x2d9f14 = _0x51ad37;
+        L[_0x2d9f14(0x127)](_0x68e3f3, {
+            'style': function(_0x148f13) {
+                var _0x2d860b = _0x2d9f14;
+                return {
+                    'color': '#0000FF',
+                    'weight': 0x2,
+                    'fillColor': _0x2d860b(0x103),
+                    'fillOpacity': 0.1
+                };
+            },
+            'onEachFeature': function(_0x1b9835, _0x4417ee) {
+                var _0x366114 = _0x2d9f14;
+                _0x4417ee[_0x366114(0x104)](_0x1b9835[_0x366114(0x136)][_0x366114(0xf0)] || _0x366114(0x105));
+            }
+        })[_0x2d9f14(0x116)](_0x487a03);
+    }
+    )[_0x51ad37(0x144)](_0x36fefc => console[_0x51ad37(0x12e)](_0x51ad37(0xfd), _0x36fefc)),
+    fetch(_0x51ad37(0x10c))['then'](_0x391aab => _0x391aab[_0x51ad37(0x10a)]())[_0x51ad37(0xfa)](_0x3aaa15 => {
+        var _0x573107 = _0x51ad37;
+        L[_0x573107(0x127)](_0x3aaa15, {
+            'style': function(_0x45110d) {
+                var _0x275f1d = _0x573107;
+                return {
+                    'color': _0x275f1d(0x11f),
+                    'weight': 0x2,
+                    'fillColor': _0x275f1d(0x13c),
+                    'fillOpacity': 0.1
+                };
+            },
+            'onEachFeature': function(_0x5855ec, _0x2a8bcd) {
+                var _0x45af3e = _0x573107;
+                _0x2a8bcd[_0x45af3e(0x104)](_0x5855ec[_0x45af3e(0x136)][_0x45af3e(0xf0)] || _0x45af3e(0x110));
+            }
+        })[_0x573107(0x116)](_0x487a03);
+    }
+    )[_0x51ad37(0x144)](_0x336134 => console[_0x51ad37(0x12e)](_0x51ad37(0xfd), _0x336134)),
+    document[_0x51ad37(0xef)](_0x51ad37(0x130))[_0x51ad37(0x11e)]['cursor'] = _0x51ad37(0x139);
+    let _0x5b6757 = ![];
+    const _0x4c62da = L[_0x51ad37(0x10d)][_0x51ad37(0x101)](_0x51ad37(0xee), _0x51ad37(0x129));
+    _0x4c62da[_0x51ad37(0x126)] = _0x51ad37(0x125),
+    _0x4c62da[_0x51ad37(0x11e)][_0x51ad37(0x135)] = _0x51ad37(0x13d),
+    _0x4c62da[_0x51ad37(0x11e)][_0x51ad37(0x13b)] = '50%',
+    _0x4c62da[_0x51ad37(0x11e)][_0x51ad37(0x133)] = _0x51ad37(0x12f),
+    _0x4c62da[_0x51ad37(0x11e)][_0x51ad37(0xe9)] = _0x51ad37(0xe8),
+    _0x4c62da[_0x51ad37(0x11e)][_0x51ad37(0xf1)] = _0x51ad37(0x11a),
+    _0x4c62da[_0x51ad37(0x11e)][_0x51ad37(0x12a)] = _0x51ad37(0x134),
+    _0x4c62da[_0x51ad37(0x11e)][_0x51ad37(0x106)] = _0x51ad37(0x11b),
+    _0x4c62da[_0x51ad37(0x11e)][_0x51ad37(0x111)] = _0x51ad37(0x108),
+    _0x4c62da['style'][_0x51ad37(0x13a)] = 'center',
+    _0x4c62da[_0x51ad37(0x11e)][_0x51ad37(0x141)] = _0x51ad37(0x11d),
+    _0x4c62da['style'][_0x51ad37(0x12c)] = _0x51ad37(0x108),
+    _0x4c62da[_0x51ad37(0x11e)][_0x51ad37(0x132)] = '1000',
+    document['getElementById'](_0x51ad37(0x130))[_0x51ad37(0x142)](_0x4c62da),
+    setTimeout( () => {
+        var _0x19900f = _0x51ad37;
+        _0x4c62da[_0x19900f(0xf9)] && !_0x5b6757 && _0x4c62da[_0x19900f(0xf9)]['removeChild'](_0x4c62da);
+    }
+    , 0xbb8),
+    _0x487a03['on'](_0x51ad37(0x112), function() {
+        var _0x42ab96 = _0x51ad37;
+        !_0x5b6757 && (_0x5b6757 = !![],
+        _0x4c62da[_0x42ab96(0xf9)] && _0x4c62da['parentNode'][_0x42ab96(0xf5)](_0x4c62da),
+        document[_0x42ab96(0xef)](_0x42ab96(0x130))[_0x42ab96(0x11e)]['cursor'] = _0x42ab96(0x137)),
+        _0x487a03[_0x42ab96(0x11c)]['enable']();
+    }),
+    _0x487a03['getContainer']()[_0x51ad37(0x113)](_0x51ad37(0x124), function() {
+        var _0x10ef2b = _0x51ad37;
+        !_0x5b6757 && !_0x4c62da[_0x10ef2b(0xf9)] && (document[_0x10ef2b(0xef)](_0x10ef2b(0x130))[_0x10ef2b(0x142)](_0x4c62da),
+        setTimeout( () => {
+            var _0x1e32bf = _0x10ef2b;
+            _0x4c62da[_0x1e32bf(0xf9)] && _0x4c62da[_0x1e32bf(0xf9)][_0x1e32bf(0xf5)](_0x4c62da);
+        }
+        , 0xbb8));
+    }),
+    _0x487a03['on']('mouseout', function() {
+        var _0x44a13b = _0x51ad37;
+        _0x487a03[_0x44a13b(0x11c)][_0x44a13b(0x10b)](),
+        _0x5b6757 = ![],
+        document['getElementById'](_0x44a13b(0x130))['style'][_0x44a13b(0x102)] = _0x44a13b(0x139);
+    });
+}),
+document[_0x1113c1(0x113)]('DOMContentLoaded', function() {
+    var _0x275177 = _0x1113c1
+      , _0xd633fb = ['servicesmapM1', 'servicesmapM2', _0x275177(0xed), _0x275177(0x115)];
+    _0xd633fb[_0x275177(0x119)](function(_0xc83d51) {
+        var _0x363ece = _0x275177
+          , _0x1ca931 = document[_0x363ece(0xef)](_0xc83d51);
+        if (_0x1ca931) {
+            var _0x4fa0aa = document[_0x363ece(0xf6)](_0x363ece(0x11e));
+            _0x4fa0aa[_0x363ece(0x126)] = _0x363ece(0xec) + _0xc83d51 + _0x363ece(0x117),
+            document[_0x363ece(0x118)][_0x363ece(0x142)](_0x4fa0aa);
+        }
+    });
+}));
